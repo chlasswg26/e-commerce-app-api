463b3fa2710c0e45485d9debd1ff2500ac06d3fc
Ichlas Wardy Gustama <cahyo4713@gmail.com>
2021-09-24 23:30:44 +0700

    Fix redis cache

--- src/config/redis.js
+++ src/config/redis.js
@@ -5,15 +5,24 @@ const {
   REDIS_HOST,
   REDIS_PASSWORD,
   REDIS_TLS,
+  REDIS_URL,
   NODE_ENV
 } = process.env
 
-const redisClient = redis.createClient({
+let redisOptions = {
   host: REDIS_HOST || '127.0.0.1',
   port: REDIS_PORT || 6379,
   password: REDIS_PASSWORD || undefined,
   tls: NODE_ENV === 'production' ? REDIS_TLS : false
-})
+}
+
+if (NODE_ENV === 'production') {
+  redisOptions = {
+    url: REDIS_URL
+  }
+}
+
+const redisClient = redis.createClient(redisOptions)
 
 if (NODE_ENV === 'development') {
   redisClient.on('error', (error) => {
